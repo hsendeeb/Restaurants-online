e98b5bf6dd6af3d5b274ee2ab1f1180a427616da
hsendeeb <hsendeeb2@gmail.com>
2025-07-26 23:08:55 +0300

    Add files via upload

---
+++ admin/components/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Restaurant Admin</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-5">
+                        <li class="nav-item mx-4"><a class="text-decoration-none text-light" href="./dashboard.php">Home</a></li>
+                        <li class="nav-item mx-4"><a class="text-decoration-none text-light" href="./clients.php">Clients</a></li>
+                        <li class="nav-item mx-4"><a class="text-decoration-none text-light" href="./deliveryMan.php">delivery man</a></li>
+                    </ul>
+                </div>
+                </div>
+        </nav>
